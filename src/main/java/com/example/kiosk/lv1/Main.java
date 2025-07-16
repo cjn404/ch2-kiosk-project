988b834f36b7881263da95d5bba67c416d4eacea
cjn404 <moi404@naver.com>
2025-07-16 22:39:34 +0900

    chore: replace w with ₩ in lv1 main

--- src/main/java/com/example/kiosk/lv1/Main.java
+++ src/main/java/com/example/kiosk/lv1/Main.java
@@ -14,10 +14,10 @@ public class Main {
 
         while (running) {
             System.out.println("\n[ SHAKESHACK MENU ]\n" +
-                    "1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n" +
-                    "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n" +
-                    "3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\n" +
-                    "4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n" +
+                    "1. ShackBurger   | ₩ 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n" +
+                    "2. SmokeShack    | ₩ 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n" +
+                    "3. Cheeseburger  | ₩ 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\n" +
+                    "4. Hamburger     | ₩ 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n" +
                     "0. 종료      | 종료\n");
             System.out.print("위 메뉴판을 보시고 메뉴를 골라 입력해주세요: ");
 
@@ -33,20 +33,20 @@ public class Main {
             if (choice >= 0 && choice <= 4) {
                 switch (choice) {
                     case 1:
-                        System.out.println("선택한 메뉴: ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
-                        System.out.println("주문이 완료되었습니다. 금액은 W 6.9 입니다.");
+                        System.out.println("선택한 메뉴: ShackBurger   | ₩ 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거");
+                        System.out.println("주문이 완료되었습니다. 금액은 ₩ 6.9 입니다.");
                         break;
                     case 2:
-                        System.out.println("선택한 메뉴: SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
-                        System.out.println("주문이 완료되었습니다. 금액은 W 8.9 입니다.");
+                        System.out.println("선택한 메뉴: SmokeShack    | ₩ 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거");
+                        System.out.println("주문이 완료되었습니다. 금액은 ₩ 8.9 입니다.");
                         break;
                     case 3:
-                        System.out.println("선택한 메뉴: Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
-                        System.out.println("주문이 완료되었습니다. 금액은 W 6.9 입니다.");
+                        System.out.println("선택한 메뉴: Cheeseburger  | ₩ 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거");
+                        System.out.println("주문이 완료되었습니다. 금액은 ₩ 6.9 입니다.");
                         break;
                     case 4:
-                        System.out.println("선택한 메뉴: Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
-                        System.out.println("주문이 완료되었습니다. 금액은 W 5.4 입니다.");
+                        System.out.println("선택한 메뉴: Hamburger     | ₩ 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거");
+                        System.out.println("주문이 완료되었습니다. 금액은 ₩ 5.4 입니다.");
                         break;
                     case 0:
                         System.out.println("프로그램을 종료합니다.");
