10980d34ea7c4734a73ef6d9359267b2a3b60d6b
cjn404 <moi404@naver.com>
2025-07-16 21:17:23 +0900

    feat: create menuitem class and add list in lv2 main

---
+++ src/main/java/com/example/kiosk/lv2/Main.java
@@ -0,0 +1,61 @@
+package com.example.kiosk.lv2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, Kiosk!");
+
+        List<MenuItem> menuList = new ArrayList<>();
+
+        menuList.add(new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
+        menuList.add(new MenuItem("SmokeShack", 8.9,  "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuList.add(new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuList.add(new MenuItem("Hamburger",5.4, "비프패티를 기반으로 야채가 들어간 기본버거"));
+
+        Scanner scanner = new Scanner(System.in); // 반복문 밖에서 한번만
+
+        boolean running = true;
+
+        int choice = 0; // try-catch, if 문 밖에서 선언
+
+        while (running) {
+            System.out.println("\n[ SHAKESHACK MENU ]");
+            for (int i = 0; i < menuList.size(); i++) {
+                System.out.print((i + 1) + ". ");
+                menuList.get(i).printMenu();
+            }
+            System.out.println("0. 종료      | 종료\n");
+//                    "1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거\n" +
+//                    "2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거\n" +
+//                    "3. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거\n" +
+//                    "4. Hamburger     | W 5.4 | 비프패티를 기반으로 야채가 들어간 기본버거\n" +
+//                    "0. 종료      | 종료\n");
+            System.out.print("위 메뉴판을 보시고 메뉴를 골라 입력해주세요: ");
+
+            // 숫자 아닌 값(예외) 처리
+            try {
+                choice = scanner.nextInt();
+            } catch (Exception e) {
+                System.out.println("올바른 숫자(0~4)를 입력하세요.");
+                scanner.nextLine(); // 잘못된 입력 버퍼 비우기 -> 필수
+                continue;
+            }
+
+            // if (choice >= 1 && choice <= 4)
+            if (choice >= 1 && choice <= menuList.size()) {
+                MenuItem selectedItem = menuList.get(choice - 1);
+                System.out.println("선택한 메뉴: " + selectedItem.name + " | ₩ " + selectedItem.price + " | " + selectedItem.description);
+                System.out.println("주문이 완료되었습니다. 금액은 ₩ " + selectedItem.price + " 입니다.");
+            } else if (choice == 0) {
+                System.out.println("프로그램을 종료합니다.");
+                running = false;
+                break;
+            } else {
+                System.out.println("올바른 숫자(0~4)를 입력하세요.");
+            }
+        }
+    }
+}
