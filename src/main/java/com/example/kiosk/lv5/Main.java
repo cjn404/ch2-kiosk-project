2236406f5b8517adf14271e220692ea8b2d4fbb0
cjn404 <moi404@naver.com>
2025-07-22 11:21:48 +0900

    refactor: encapsulate lv4 into new lv5

---
+++ src/main/java/com/example/kiosk/lv5/Main.java
@@ -0,0 +1,11 @@
+package com.example.kiosk.lv5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Kiosk 클래스(타입)의 kiosk 객체 생성 및 생성자 실행
+        Kiosk kiosk = new Kiosk();
+        // 생성된 kiosk 객체의 start() 메소드 호출
+        kiosk.start();
+    }
+}
